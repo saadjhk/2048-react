c6c31a5afa88837ae3f2f6e10186275a7d60db0a
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-02-25 22:39:53 +0500

    board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useGameLogic } from "./context/2048";
 
 function App() {
-    const { gameState, onSwipeRight } = useGameLogic();
+    const { gameState, onSwipeRight, onSwipeDown, onSwipeLeft, onSwipeUp } = useGameLogic();
+    const [selectedCell, setSelectedCell] = useState({ row: -1, column: -1 });
 
-    console.log(gameState);
-    return <div className="App">{gameState} <button onClick={() => onSwipeRight ? onSwipeRight(2,3) : null}>Swipe</button></div>;
+    return (
+        <div className="App">
+            <div>
+                {gameState.map((row) => {
+                    return <div>{row.map((stateVal) => (isNaN(stateVal) ? "-" : stateVal)).join(" ")}</div>;
+                })}
+            </div>
+
+            <div>
+                <input type="number" name="input-row" id="input-row" placeholder="Row" />
+                <input type="number" name="input-column" id="input-column" placeholder="Column" />
+            </div>
+            <button onClick={() => (onSwipeUp ? onSwipeUp(2, 3) : null)}>Swipe Up</button>
+            <button onClick={() => (onSwipeDown ? onSwipeDown(2, 3) : null)}>Swipe Down</button>
+            <button onClick={() => (onSwipeLeft ? onSwipeLeft(2, 3) : null)}>Swipe Left</button>
+            <button onClick={() => (onSwipeRight ? onSwipeRight(2, 3) : null)}>Swipe Right</button>
+        </div>
+    );
 }
 
 export default App;
